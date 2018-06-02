2562f2e7b01d52b870f0b26719955ad36b26b687
Katharine Li <kli991221@gmail.com>
2018-06-02 19:04:20 -0400

    s

---
+++ octopus/octopus_script.js
@@ -0,0 +1,56 @@
+function changeClass(){
+	document.getElementById("mina").className = "card mina"; 
+	var b = document.getElementById("body");
+		b.style.background = "#BF1153";
+	var x = document.getElementById("minaa");
+        x.style.visibility = "hidden";
+}
+function changeClass2(){
+	document.getElementById("vic").className = "card vic";
+	var b = document.getElementById("body");
+		b.style.background = "#B4D8C3";
+	var x = document.getElementById("vicc");
+        x.style.visibility = "hidden";
+}
+function changeClass3(){
+	document.getElementById("spence").className = "card spence";
+	var b = document.getElementById("body");
+		b.style.background = "#D5D748";
+	var x = document.getElementById("spencee");
+        x.style.visibility = "hidden";
+}
+function changeClass4(){
+	document.getElementById("fred").className = "card fred";
+	var b = document.getElementById("body");
+		b.style.background = "#D242E3";
+	var x = document.getElementById("fredd");
+        x.style.visibility = "hidden";
+}
+function changeClass5(){
+	document.getElementById("kat").className = "card kat";
+	var b = document.getElementById("body");
+		b.style.background = "#00A5A0";
+	var x = document.getElementById("katt");
+        x.style.visibility = "hidden";
+}
+function changeClass6(){
+	document.getElementById("josh").className = "card josh";
+	var b = document.getElementById("body");
+		b.style.background = "#2AE205";
+	var x = document.getElementById("joshh");
+        x.style.visibility = "hidden";
+}
+function changeClass7(){
+	document.getElementById("noah").className = "card noah";
+	var b = document.getElementById("body");
+		b.style.background = "#F088D5";
+	var x = document.getElementById("noahh");
+        x.style.visibility = "hidden";
+}
+function changeClass8(){
+	document.getElementById("zhang").className = "card zhang";
+	var b = document.getElementById("body");
+		b.style.background = "#FFB31C";
+	var x = document.getElementById("zhangg");
+        x.style.visibility = "hidden";
+}
